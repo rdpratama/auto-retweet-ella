5472cbfbae858efa7d6d83220de130857476c32c
platipus-x <rivanzaxa@gmail.com>
2022-08-24 02:28:44 +0700

    ella lucu ya

---
+++ main.py
@@ -0,0 +1,36 @@
+import tweepy as twitter
+import config
+import random
+import time, datetime
+import json
+
+client = twitter.Client(bearer_token=config.BEARER_TOKEN ,consumer_key=config.API_KEY, consumer_secret=config.API_SECRET, access_token=config.ACCESS_TOKEN, access_token_secret=config.ACCESS_TOKEN_SECRET)
+
+screen_name = 'AM_EllaJKT48'
+
+user_id = '1487291928257654785'
+
+response = client.get_users_tweets(user_id, )
+
+# By default, only the ID and text fields of each Tweet will be returned
+# for tweet in response.data:
+#    print(tweet.id)
+ #   print(tweet.text)
+
+
+def retweet_ella(delay):
+    print(f"\n{datetime.datetime.now()}\n")
+
+    while True:
+        for tweet in response.data:
+            print(tweet.id)
+            print(tweet.text)
+            client.retweet(tweet.id)
+        time.sleep(120)
+retweet_ella(120)
+
+
+
+
+
+
